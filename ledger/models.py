3906f0819b3e2f7972699dfa459670aa4491d2a7
Luis Andrei Pagkaliwangan <lapwenggan@gmail.com>
2026-02-19 16:23:24 +0800

    Finished models.py. Completed ForeignKey for ingredient and recipe. related_name of ingredient is recipe, and for recipe, it is ingredients. Added function to get_absolute_url via reverse function for classes, Ingredient and Recipe. RecipeIngredient class is equivalent to TaskGroup from our discussions. It holds the quantity of ingredients for a recipe.

--- ledger/models.py
+++ ledger/models.py
@@ -3,27 +3,41 @@ from django.urls import reverse
 # Create your models here.
 
 class Ingredient(models.Model):
-    name = models.CharField()
+    name = models.CharField(max_length=200)
 
     def __str__(self):
         return self.name
+    
+    def get_absolute_url(self):
+        return reverse('ledger:ingredient_detail', args=[str(self.id)])
+
 
 class Recipe(models.Model):
-    name = models.CharField()
+    name = models.CharField(max_length=200)
 
     def __str__(self):
         return self.name
+    
+    def get_absolute_url(self):
+        return reverse('ledger:recipe_detail', args=[str(self.id)])
+
 
 class RecipeIngredient(models.Model):
-    name = models.CharField()
+    quantity = models.CharField(max_length=200)
 
     ingredient = models.ForeignKey(
         Ingredient,
+        on_delete=models.CASCADE,
+        related_name='recipe',
     )
 
     recipe = models.ForeignKey(
         Recipe,
+        on_delete=models.CASCADE,
+        related_name='ingredients'
     )
 
     def __str__(self):
-        return self.name
+        return f"{self.quantity} of {self.ingredient.name} for {self.recipe.name}."
+    
+    
