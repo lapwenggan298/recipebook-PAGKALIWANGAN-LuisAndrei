d46a0798b3a6be73e8ae00d889d94d681f54537f
Luis Andrei Pagkaliwangan <lapwenggan@gmail.com>
2026-02-19 17:36:54 +0800

    Added the new file created by the migrations of the models. Modified admin.py, added an admin panel that allows me to add and edit the models (its contents). Modified recipe_list.html to mirror that of the task_list.html Modified views.py where there is a commented out function for rendering. I will be using/attempting to use class based views as they appear cleaner. Modified urls.py where they will "call" the class based views. Also deleted old recipe_1 and recipe_2 htmls. They will be replaced by recipe_detail.html soon.

--- ledger/admin.py
+++ ledger/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+
 from .models import Ingredient, Recipe, RecipeIngredient
 # Register your models here.
+
+class RecipeIngredientInLine(admin.TabularInline):
+    model = RecipeIngredient
+
+
+class RecipeAdmin(admin.ModelAdmin):
+    model = Recipe
+    inlines = [RecipeIngredientInLine]
+
+
+class IngredientAdmin(admin.ModelAdmin):
+    model = Ingredient
+    search_fields = ['name']
+    list_display = ['name']
+
+
+admin.site.register(Recipe, RecipeAdmin)
+admin.site.register(Ingredient, IngredientAdmin)
--- ledger/views.py
+++ ledger/views.py
@@ -1,137 +1,31 @@
 from django.shortcuts import render
-from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+
+from .models import Recipe
 # Create your views here.
 
-def recipe_list(request):
-    ctx = {
-        "recipes": [
-        {
-            "name": "Recipe 1",
-            "ingredients": [
-                {
-                    "name": "tomato",
-                    "quantity": "3pcs"
-                },
-                {
-                    "name": "onion",
-                    "quantity": "1pc"
-                },
-                {
-                    "name": "pork",
-                    "quantity": "1kg"
-                },
-                {
-                    "name": "water",
-                    "quantity": "1L"
-                },
-                {
-                    "name": "sinigang mix",
-                    "quantity": "1 packet"
-                }
-            ],
-            "link": "recipe/1"
-        },
-        {
-            "name": "Recipe 2",
-            "ingredients": [
-                {
-                    "name": "garlic",
-                    "quantity": "1 head"
-                },
-                {
-                    "name": "onion",
-                    "quantity": "1pc"
-                },
-                {
-                    "name": "vinegar",
-                    "quantity": "1/2cup"
-                },
-                {
-                    "name": "water",
-                    "quanity": "1 cup"
-                },
-                {
-                    "name": "salt",
-                    "quantity": "1 tablespoon"
-                },
-                {
-                    "name": "whole black peppers",
-                    "quantity": "1 tablespoon"
-                },
-                {
-                    "name": "pork",
-                    "quantity": "1 kilo"
-                }
-            ],
-            "link": "recipe/2"
-        }
-    ]
-    }
-    return render(request, 'recipe_list.html', ctx)
+class RecipeListView(ListView):
+    model = Recipe
+    template_name = 'recipe_list.html'
 
-def recipe_1(request):
-    ctx = {
-        "name": "Recipe 1",
-                    "ingredients": [
-                        {
-                            "name": "tomato",
-                            "quantity": "3pcs"
-                        },
-                        {
-                            "name": "onion",
-                            "quantity": "1pc"
-                        },
-                        {
-                            "name": "pork",
-                            "quantity": "1kg"
-                        },
-                        {
-                            "name": "water",
-                            "quantity": "1L"
-                        },
-                        {
-                            "name": "sinigang mix",
-                            "quantity": "1 packet"
-                        }
-                    ],
-                    "link": "recipe/1"
-    }
-    return render(request, 'recipe_1.html', ctx)
 
-def recipe_2(request):
-    ctx = {
-        "name": "Recipe 2",
-            "ingredients": [
-                {
-                    "name": "garlic",
-                    "quantity": "1 head"
-                },
-                {
-                    "name": "onion",
-                    "quantity": "1pc"
-                },
-                {
-                    "name": "vinegar",
-                    "quantity": "1/2cup"
-                },
-                {
-                    "name": "water",
-                    "quanity": "1 cup"
-                },
-                {
-                    "name": "salt",
-                    "quantity": "1 tablespoon"
-                },
-                {
-                    "name": "whole black peppers",
-                    "quantity": "1 tablespoon"
-                },
-                {
-                    "name": "pork",
-                    "quantity": "1 kilo"
-                }
-            ],
-            "link": "recipe/2"
-    }
-    return render(request, 'recipe_2.html', ctx)
+class RecipeDetailView(DetailView):
+    model = Recipe
+    template_name = 'recipe_list.html'
+
+#def recipe_list(request)
+    # recipes = Recipe.objects.all()
+    # ctx = {
+    #     "recipes": recipes
+    # }
+    # return render(request, 'recipe_list.html', ctx)
+
+
+# def recipe_detail(request,pk):
+#   recipe = Recipe.objects.get(pk=pk)
+#     ctx = {
+#           "recipe":recipe         
+#    }
+#     return render(request, 'recipe_detail.html', ctx)
+
