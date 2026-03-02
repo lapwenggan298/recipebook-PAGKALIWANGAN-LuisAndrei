2a0b194e2523a5912707d3e6e67819dd22a08f21
Luis Andrei Pagkaliwangan <lapwenggan@gmail.com>
2026-03-02 22:27:29 +0800

    Added Author Foreign Key in Recipe. Also added the CreatedOn DateTime Field, as well as the UpdatedOn DateTime Field.

--- ledger/models.py
+++ ledger/models.py
@@ -30,6 +30,15 @@ class Ingredient(models.Model):
 class Recipe(models.Model):
     name = models.CharField(max_length=200)
 
+    author = models.ForeignKey(
+        Profile,
+        on_delete=models.CASCADE,
+        related_name='recipes',
+    )
+
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
     def __str__(self):
         return self.name
     
