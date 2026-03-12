fca190fcc81c200ea2ae39f3af5c82f1303751c6
Luis Andrei Pagkaliwangan <lapwenggan@gmail.com>
2026-03-12 22:43:20 +0800

    Added stuff to project level settings.py
    Added:
    STATICFILES_DIRS
    MEDIA_URL
    MEDIA_ROOT

--- recipebook/settings.py
+++ recipebook/settings.py
@@ -120,6 +120,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR/'static',
+]
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR/'media'
+
 LOGIN_REDIRECT_URL = '/recipes/list'
 LOGOUT_REDIRECT_URL = '/login/'
 LOGIN_URL = '/login/'
