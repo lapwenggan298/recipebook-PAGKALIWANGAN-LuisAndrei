0c4be0583fabed3915a4a2440bb12d94743cbc1b
Luis Andrei Pagkaliwangan <lapwenggan@gmail.com>
2026-03-12 22:32:13 +0800

    Added new files and modified urls.py to comply.
    Files to comply with suggestions/comments from lab 3.
    Did not put them last lab 3 as it was not fully discussed to us and that we could just use the default by Django anyway.
    
    project level urls.py
    Removed old manual/"tingi-tingi"/isa-isa paths.
    Added the necessary thing for static files.

--- recipebook/urls.py
+++ recipebook/urls.py
@@ -16,15 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('ledger.urls')),
-    path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('password_reset', auth_views.PasswordResetView.as_view(), name='password_reset'),
-    path('password_reset/done', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
-    path('reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
